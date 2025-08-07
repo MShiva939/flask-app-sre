7ff7369ec1929dce339b6c229395d14c0ae25804
MShiva939 <mdshiva125@gmail.com>
2025-08-07 21:08:10 +0530

    Updated docker pull start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,5 +6,6 @@ REPOSITORY_URI=$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/devops-app
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPOSITORY_URI
 docker stop my-app || true
 docker rm my-app || true
+docker pull $REPOSITORY_URI:latest
 docker run -d -p 80:80 --name my-app $REPOSITORY_URI:latest
 
