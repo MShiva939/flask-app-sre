3066d6dd5b3d144fd16ae614c16a22b39f9d709e
MShiva939 <mdshiva125@gmail.com>
2025-08-07 18:12:45 +0530

    updated aws creds

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 REGION=us-east-1
+REPOSITORY_URI=$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/devops-app
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPOSITORY_URI
 docker stop my-app || true
 docker rm my-app || true
-docker run -d -p 80:80 --name my-app $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/devops-app:latest
+docker run -d -p 80:80 --name my-app $REPOSITORY_URI:latest
 
