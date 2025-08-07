18423f61be7c5dc06b2cd2f3652975e908f39d5d
MShiva939 <mdshiva125@gmail.com>
2025-08-07 17:57:54 +0530

    updated the code

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 REGION=us-east-1
-docker stop myapp || true
-docker rm myapp || true
-docker run -d -p 80:80 --name myapp $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/devops-app:latest
+docker stop my-app || true
+docker rm my-app || true
+docker run -d -p 80:80 --name my-app $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/devops-app:latest
 
